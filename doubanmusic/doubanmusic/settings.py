8dace6758a9db195dbf110a4d0f71d14b0c02309
MuZeRoFSo <37204705+MuZeRoFso@users.noreply.github.com>
2020-07-27 23:55:16 +0800

    添加随机请求头
    添加rotate_useragent.py文件，并设置settings.py文件，完成随机用户代理功能

--- doubanmusic/doubanmusic/settings.py
+++ doubanmusic/doubanmusic/settings.py
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#     'doubanmusic.middlewares.DoubanmusicDownloaderMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    'doubanmusic.middlewares.DoubanmusicDownloaderMiddleware': 543,
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+    'doubanmusic.rotate_useragent.RotateUserAgentMiddleware': 400
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
